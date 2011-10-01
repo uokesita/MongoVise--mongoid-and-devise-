58b39de0932c96f4f2d9c7dd15b080416017b552
uokesita <osledybazo@gmail.com>
2011-10-01 00:09:15 -0430

    initial comit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codersla::Application.config.secret_token = '62754e17b3929b5d3f85d19063bd714ce1228865117c37465e09a0a3407ff8d832c4a3fd67f1249bb7a303e47154163bbf7beaa8888c6608faf7f1a9aebe3231'
